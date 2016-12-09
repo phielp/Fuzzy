02858c2d85227875f19d92a50f16a49548d3a4f7
eevlogieva <eevlogieva@yahoo.com>
2016-12-09 18:37:38 +0200

    Added some comments

--- parser.py
+++ parser.py
@@ -1,72 +1,51 @@
-# coding: utf-8
-# f = open('booksummaries.txt', 'r')
-# books1 = []
-# books2 = []
-# t = open('BX-Books.csv', 'r')
-# line = f.readline()
-# while line:
-#     els = line.split('\t')
-#     books1.append(els[2])
-#     line = f.readline()
-
-# line = t.readline()
-# while line:
-#     els = line.split(';')
-#     books2.append(els[2].strip('"'))
-#     line = t.readline()
-# #print len([book for book in books1 if book in books2])
-
+# the parameters that the script takes is the starting index (from where to scrape)
+# and the number of books to scrape at once
 import requests
-import sys
 import traceback
 import time
 from lxml import html
 import datetime
-import xml.etree.ElementTree as ElementTree
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-START_INDEX = 7504
-NUMBER_OF_BOOKS_AT_ONCE = 10000
 
-t = open('BX-Books.csv', 'r')
-new_file = open('books_with_genres3.csv', 'w')
-t.readline()
-starttime = datetime.datetime.now()
-for j in range(START_INDEX):
+def scrape_genres(start_index, number_of_books_at_once):
+    # open the file with the books for reading, and a new one to write the scraped info
+    t = open('BX-Books.csv', 'r')
+    filename = 'books_with_genres' + str(start_index) + '.csv'
+    new_file = open(filename, 'w')
     t.readline()
-for i in range(START_INDEX, START_INDEX+NUMBER_OF_BOOKS_AT_ONCE):
-    try:
-        line = t.readline()
-        els = line.split(';')
-        isbn = els[0].strip('"')
-        url = "https://www.goodreads.com/search?q=" + isbn
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
-        page = requests.get(url, headers=headers)
-        root = html.document_fromstring(page.text)
-        #e = root.xpath('.//a[@class="actionLinkLite bookPageGenreLink"]')
-        genresList = root.xpath('.//a[@class="actionLinkLite greyText bookPageGenreLink"]')
-        etexts = [ei.attrib['title'] for ei in genresList]
-        if len(etexts) > 5:
-            while len(etexts) > 5:
-                del etexts[5]
-        newline = str(i) + ';' + str(isbn) + ';' + str(etexts) + '\n'
-        new_file.write(newline)
-        time.sleep(0.1)
-    except Exception:
-        print datetime.datetime.now() - starttime
-        print i
-        traceback.print_exc()
-        t.close()
-        new_file.close()
-        sys.exit()
+    starttime = datetime.datetime.now()
+    # the scrip will be started more than once, so that we have to first navigate to the start_index
+    for j in range(start_index):
+        t.readline()
+    for i in range(start_index, start_index+number_of_books_at_once):
+        # try/except is used because we want in any kind of mistake (network or keyboard interrupt
+        # for example) to save the sata we have scraped so far
+        try:
+            line = t.readline()
+            els = line.split(';')
+            isbn = els[0].strip('"')
+            url = "https://www.goodreads.com/search?q=" + isbn
+            headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+            page = requests.get(url, headers=headers)
+            root = html.document_fromstring(page.text)
+            #e = root.xpath('.//a[@class="actionLinkLite bookPageGenreLink"]')
+            genresList = root.xpath('.//a[@class="actionLinkLite greyText bookPageGenreLink"]')
+            etexts = [ei.attrib['title'] for ei in genresList]
+            # limit the number of genres to 5, because otherwise we have a lot of books
+            # with very low membership degree to a certain genre
+            if len(etexts) > 5:
+                while len(etexts) > 5:
+                    del etexts[5]
+            newline = str(i) + ';' + str(isbn) + ';' + str(etexts) + '\n'
+            new_file.write(newline)
+            time.sleep(0.1)
+        except Exception:
+            print datetime.datetime.now() - starttime
+            print i
+            traceback.print_exc()
+            t.close()
+            new_file.close()
+    print datetime.datetime.now() - starttime
 
-# f = open('a.html', 'w')
-# f.write(page.text)
-
-
-# etexts = [ei.text for ei in e]
-# genres = set(etexts)
-# print genres
-
-print datetime.datetime.now() - starttime
+if __name__ == '__main__':
+    scrape_genres(sys.argv[1], sys.argv[2])
